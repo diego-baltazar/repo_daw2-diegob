6046b7086549154f698efb18201cd12787e06076
diego.baltazarmorales <diego.baltazarmorales@gmail.com>
2021-05-23 15:52:16 -0500

    actualiza

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -1,11 +1,13 @@
 package com.empresa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,22 @@ public class AlumnoController {
 		}else {
 			return ResponseEntity.badRequest().build();
 		}
+	}
+	
+	@PutMapping
+	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj){
+		log.info(">>>>> actualiza : " + obj.getIdAlumno() + " <<<<<");
+		Optional<Alumno> optAlumno = service.obtienePorId(obj.getIdAlumno());
+		if(optAlumno.isPresent()) {
+			Alumno objSalida = service.insertaActualizaAlumno(obj);
+			if(objSalida != null) {
+				return ResponseEntity.ok(objSalida);
+			}else {
+				return ResponseEntity.badRequest().build();
+			}
+		}else {
+			log.info(">>>>> no existe el id : " + obj.getIdAlumno() + " <<<<<");
+			return ResponseEntity.badRequest().build();
+		}	
 	}
 }
